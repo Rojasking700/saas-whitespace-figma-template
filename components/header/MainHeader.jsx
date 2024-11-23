67f75c0e0ab03bb1a6ffc443f52c688dc6aba09e
Rojasking700 <grojaspiano@gmail.com>
2024-11-23 17:24:34 -0600

    second commit

--- components/header/MainHeader.jsx
+++ components/header/MainHeader.jsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 export default function MainHeader() {
   return (
-    <div>
+    <div className="ps-header">
       <div className="ps-header-logo">
         <Link href={'/'} >
-          {/* <Image 
-          /> */}
-          klj
+          <Image 
+            src="/imgs/logos/Logo.png"
+            alt="Whitespace logo"
+            width={191}
+            height={34}
+          />
         </Link>
       </div>
       
