0e1762f346b119586d37c15243974f8577779b63
Rojasking700 <grojaspiano@gmail.com>
2024-11-23 18:05:21 -0600

    clean up

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,9 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import MainHeader from "@/components/header/MainHeader";
 import '../scss/styles.scss';
 
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -25,7 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
       <body className={`${inter.className}`}>
         <MainHeader />
         {children}
