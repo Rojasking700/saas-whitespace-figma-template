1b4d89755e7522de6fcb4c2597e9d9415d408716
Rojasking700 <grojaspiano@gmail.com>
2024-11-23 20:13:27 -0600

    finish hero

--- components/LandingPage/HeroSection.jsx
+++ components/LandingPage/HeroSection.jsx
@@ -1,11 +1,26 @@
 
+import Image from 'next/image'
 import styles from '~/scss/LandingPage/HeroSection.module.scss'
 
-export default function HeroSection(){
+export default function HeroSection() {
   return (
     <div className={styles.HeroSection}>
-      <div>
-        hello
+      <div className={styles.heroText}>
+        <h2>
+          Get More Done with whitepace
+        </h2>
+        <p>
+          Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks
+        </p>
+        <button className={styles.heroButton} >Try Whitepace free</button>
+      </div>
+      <div className={styles.heroImg}>
+        <Image
+          src={'/imgs/hero/AnalyzeData.png'}
+          alt='Analyze Data'
+          width={600}
+          height={600}
+        />
       </div>
     </div>
   )
