7227be253bf40deaf0adbee70d8d2c35926472dd
Rojasking700 <grojaspiano@gmail.com>
2024-12-01 23:16:53 -0600

    minor clean up

--- components/LandingPage/TryToday.jsx
+++ components/LandingPage/TryToday.jsx
@@ -7,14 +7,12 @@ export default function TryToday() {
   return (
     <div className={styles.tryToday}>
       <div className={styles.innerCont}>
-
         <h1>Try Whitepace today</h1>
         <p>
           Get started for free.
           <br />
           Add your whole team as your needs grow.
         </p>
-        {/* <p></p> */}
         <button>Try Taskey free</button>
         <p>On a big team? Contact sales</p>
         <Image
